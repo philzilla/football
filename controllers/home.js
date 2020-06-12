64e8432bfc78b170d5bde28ef52d4979be1715f8
philzilla <dev@philippeyim.fr>
2020-06-12 11:15:27 +0200

    Affiche liste des joeurs avec leur club

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,8 @@
 exports.getHomePage = (req, res) => {
-    let query = "SELECT  `players.id`, `players.firstname`, `players.lastname`, `players.position`, `players.number`, `players.image`,`club.name`,`club.city`,`club.country` " + " FROM players INNER JOIN club ON  club_id = club.id;" 
-    
+    let query = "SELECT players.*, club.name, club.country FROM players INNER JOIN club ON club_id = club.id" 
+
+    // Julien
+   // let query = "SELECT * FROM players INNER JOIN club ON club_id = club.id;"
     
     db.query(query, (err, result) => {
         console.log("result :", result)
